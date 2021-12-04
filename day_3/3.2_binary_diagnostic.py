132d30aef0466be2be2d3fd11da4c0f1f50cc11b
megangraft <mgraft01@gmail.com>
2021-12-03 16:15:03 -0800

    Recursive exit condition now comes first

--- day_3/3.2_binary_diagnostic.py
+++ day_3/3.2_binary_diagnostic.py
@@ -4,14 +4,14 @@ def life_support_rating(o2, co2):
     return o2 * co2
 
 def filter(list, i=0, co2=False): # where `i` is the bit position
+    if len(list) == 1:
+        return list[0]
     count = 0
     for item in list:
         count += int(item[i])
     dom_num = 1 if count >= len(list) / 2 else 0 # prefers 1 in case of ties by default
     pref_num = dom_num if co2 == False else abs(dom_num - 1) # pref_num is opposite if co2 reading sought
     list = [x for x in list if int(x[i]) == pref_num]
-    if len(list) == 1:
-        return list[0]
     return filter(list, i + 1, co2)
 
 o2_rating = filter(f) # oxygen generator rating
